6ef22368d0761f737109c7493ffb9e6dc9d30025
elmm <elmm00@gmail.com>
2016-02-22 00:41:02 +0200

    commented out seq_dump

--- include/rtw_debug.h
+++ include/rtw_debug.h
@@ -183,9 +183,9 @@ extern void rtl871x_cedbg(const char *fmt, ...);
 	#define _dbgdump rtl871x_cedbg
 	#define _seqdump(sel, fmt, arg...) _dbgdump(fmt, ##arg)
 #elif defined PLATFORM_LINUX
-	#include <linux/seq_file.h>
+	//#include <linux/seq_file.h>
 	#define _dbgdump printk
-	#define _seqdump seq_printf
+	//#define _seqdump seq_printf
 #elif defined PLATFORM_FREEBSD
 	#define _dbgdump printf
 	#define _seqdump(sel, fmt, arg...) _dbgdump(fmt, ##arg)
